75d938f140f527be100fdfb392f68e0d59bde87e
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-10 16:56:57 +0100

    workshop challenge 2

--- Workshop_C/C1.c
+++ Workshop_C/C1.c
@@ -1,8 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-    printf("Hello world!\n");
+    printf("Hello");
+
+    char nom[20];
+    char prenom[20];
+    int age;
+    char sexe[20];
+    char tele[20];
+
+    printf("Votre Nom");
+    scanf("%s", nom);
+
+    printf("Votre Prenom");
+    scanf("%s", prenom);
+
+    printf("Votre Age");
+    scanf("%d",&age);
+
+    printf("Votre Sexe");
+    scanf("%s", sexe);
+
+    printf("Votre Tele");
+    scanf("%s", tele);
+
+    printf("Nom : %s, Prenom : %s, Age : %d, Sexe : %s, tele : %s", nom, prenom, age, sexe, tele);
+
     return 0;
 }
