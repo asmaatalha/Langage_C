63c2a0c96b894b7b4014eb4c80e9af613a2d695a
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-11 13:20:39 +0100

    challenge 7

---
+++ Workshop_C/C7.c
@@ -0,0 +1,23 @@
+int main()
+{
+    printf("Hello\n");
+
+    int a, b, mul, somme, sous;
+    float div , res;
+
+    printf("Entrer a: ");
+    scanf("%d", &a);
+
+    printf("Entrer b: ");
+    scanf("%d", &b);
+
+    mul = a * b;
+    somme = a + b;
+    sous = a - b;
+    div  = a / b;
+    res = a % b;
+
+    printf("multiplication  : %d, Somme : %d, soustraction  : %d, division  : %f, reste  : %f", mul, somme, sous, div, res);
+
+    return 0;
+}
