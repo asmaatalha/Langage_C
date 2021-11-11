b7e0ed14be60161005515d1fce3212d16b49832f
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-11 16:01:17 +0100

    challenge 11

---
+++ Workshop_C/C11.c
@@ -0,0 +1,13 @@
+int main()
+{
+    int r, Lo, La;
+
+    printf("Entrer longueur et largeur: ");
+    scanf("%d%d", &Lo, &La);
+
+    r = 2 * Lo * La;
+
+    printf("Circonférence d'un rectangle : %d", r);
+
+    return 0;
+}
