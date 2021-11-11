4a24edbc7f6c28f04079bceae867b60ed0f39f5d
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-11 09:23:48 +0100

    challenge 2

--- Workshop_C/C2.c
+++ Workshop_C/C2.c
@@ -1,7 +1,15 @@
 
 
-int main()
-{
-    printf("Hello Asmaa");
+int main(){
+    
+    float temp;
+
+    printf("Temperature en C : \n");
+    scanf("%f", &temp);
+
+    temp = (temp * 1.8) + 32;
+
+    printf("Temperature en F : %f\n", temp);
+
     return 0;
 }
