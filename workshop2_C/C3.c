86722e750e3842b6a275a5723ed2544a40bfefec
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-12 15:54:00 +0100

    update workshop2_C challenge 3

--- workshop2_C/C3.c
+++ workshop2_C/C3.c
@@ -3,6 +3,20 @@
 
 int main(){
 
+    int a, b;
+
+    printf("Entre le nomber:\n");
+    scanf("%d", &a);
+
+    b = a%2;
+    if (b == 0)
+    {
+        printf("le nomber %d est pair", a);
+    }
+    else
+    {
+        printf("le nomber %d est impair", a);
+    }
     
 
     return 0;
