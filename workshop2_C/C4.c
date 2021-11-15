6130bbe139fe89713cde13df4e579cad379fcd2e
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-15 11:28:50 +0100

    workshop2_C challenge 4

---
+++ workshop2_C/C4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    char alphapy;
+
+    printf("Entrez la voyelle: \n");
+    scanf("%s", &alphapy);
+
+    switch (alphapy)
+    {
+        case 'a':
+            printf("%c est une voyelle", alphapy);
+            break;
+        case 'e':
+            printf("%c est une voyelle", alphapy);
+            break;
+        case 'i':
+            printf("%c est une voyelle", alphapy);
+            break;
+        case 'o':
+            printf("%c est une voyelle", alphapy);
+            break;
+        case 'u':
+            printf("%c est une voyelle", alphapy);
+            break;
+        case 'y':
+            printf("%c est une voyelle", alphapy);
+            break;
+        default:
+            printf("%c est une consone");
+            break;
+    }
+       
+
+    return 0;
+}
