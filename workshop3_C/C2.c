af7908bbc6f43cf8cec06e9ef01347625279a0d5
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-16 16:06:22 +0100

    workshop3_C challenge 2

---
+++ workshop3_C/C2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int x, i, j;
+    printf("entrez un nomeb: ");
+    scanf("%d", &x);
+
+    for (i = 1; i <= x; i++)
+    {
+        for (j = x - i; j >= 1; j--)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i + i - 1; j++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+
+    }
+    
+
+
+    return 0;
+}
