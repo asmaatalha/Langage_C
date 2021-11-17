1f7ecfddc9a5367bfaeaa241a494c29753d5f700
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-17 09:46:15 +0100

    workshop3_C challenge 4

---
+++ workshop3_C/C4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int i, t[10], S = 0;
+    
+    for (i = 0; i <= 10; i++)
+    {
+        printf("entrez un nomeb: ");
+        scanf("%d", &t[i]);
+        if (t[i] == 0 || t[i] > 100)
+        {
+            break;
+          
+        }
+        
+        S = S + t[i];
+        
+    }
+    
+    printf("la somme %d", S);     
+    
+
+    return 0;
+}
