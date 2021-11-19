e903686b0fd0048a175581ae06c1c8f509cc3d33
AsmaaTalha <asmaa.talha1998@gmail.com>
2021-11-19 09:13:23 +0100

    workshop3_C challenge 7

---
+++ workshop3_C/C7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, i, A, B, C;
+    printf("Entrez un entier:\n");
+    scanf("%d", &n);
+    A = 1;
+    B = 1;
+    for(i = 2; i <= n; i++)
+    {
+       C = B + A;
+       A = B;
+       B = C;
+    }
+    if(n <= 1)
+    {
+        printf("U(%d) = 1\n",n);
+    }
+    else 
+    {
+        printf("U(%d) = %d\n",n,C);
+    }
+    
+    return 0;
+}
